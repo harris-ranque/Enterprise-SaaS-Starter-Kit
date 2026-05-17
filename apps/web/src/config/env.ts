2dea9afcc117d18ba1dae5391eda9b9f08a7f4f7
harris <goodhabitsamsam@gmail.com>
2026-05-17 16:37:28 +0800

    add: initial structure of the SaaS

---
+++ apps/web/src/config/env.ts
@@ -0,0 +1,14 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {},
+  client: {
+    NEXT_PUBLIC_API_URL: z.string().url(),
+    NEXT_PUBLIC_APP_NAME: z.string(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+  },
+});
